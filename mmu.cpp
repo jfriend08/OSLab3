70abfc530e8de5ee46bf6a8713ca6bcc60c7afc0
jfriend08 <ys486@cornell.edu>
2014-07-14 16:27:55 -0400

    --	will able to change, printp, printf

--- mmu.cpp
+++ mmu.cpp
@@ -3,6 +3,9 @@ Usage: ./mmu [-a<algo>] [-o<options>] [–f<num_frames>] inputfile randomfile
 --	detect output flag, algorithm methods, and number of frames
 --	init input, init rfile
 --	myrandom function
+--	will able to change, printp, printf
+
+Todo:	replacement algorithms is needed
 
 */
 
@@ -27,11 +30,34 @@ using namespace std;
 vector<int> randvals;
 vector<int> tasks_tmp;
 vector<vector<int> > tasks;
+vector<int> frametable;
+vector<int> fram2page_v;
 int num_frames=32, num_pages=64, O_flag=0, P_flag=0, F_flag=0, S_flag=0, p_flag=0, f_flag=0, a_flag=0; 
 int c, opterr=0, n1, n2, ofs=0;
-// char *algo;
 string ovalue, frames, arg_tmp, algo="l", fin_string;
 char fin_char;
+
+
+
+////Class////
+class PTE {
+public:
+	int PRESENT, MODIFIED, REFERENCED, PAGEDOUT, idx2frame;	
+	PTE(){
+		PRESENT=-1; REFERENCED=-1; MODIFIED=-1; PAGEDOUT=-1; idx2frame=-1;		}
+	void Zero(int rw, int page_index, int frame_index, int inst){				
+		string tmp="";
+		printf("%d: ZERO %4s %3d\n", inst ,tmp.c_str(), frame_index);	
+	}
+	void Map(int rw, int page_index, int frame_index, int inst){				
+		printf("%d: MAP %5d %3d\n", inst ,page_index, frame_index);			
+		frametable[frame_index]=page_index;
+		PRESENT=1; REFERENCED=1; MODIFIED=1; PAGEDOUT=0; idx2frame=frame_index;		
+	}
+};
+
+
+class PTE page_table[64];	
 
 ////Function////
 int myrandom(int burst, int &index) { 	
@@ -68,7 +94,6 @@ void input_init(string input1){
 			}					
 		}		
 	}
-
 	fin0.close();
 }
 ////Function////
@@ -80,6 +105,57 @@ void rfile_init(string input2){
 			int temp;fin>>temp;randvals.push_back(temp);			
 		}
 	}
+}
+void fram2page_init(int frames){
+	for (int i=0; i<frames; i++){
+		fram2page_v.push_back(-1);
+	}
+}
+void frametable_init(int frames){
+	for (int i=0; i<frames; i++){
+		frametable.push_back(-1);
+	}
+}
+////Function////
+void Zero(class PTE &page, int &){
+
+}
+
+////Function////
+void change(int rw, int page_index, int inst){	
+	if (page_table[page_index].PRESENT ==-1){
+		int frame_index=0;
+		page_table[page_index].Zero(rw, page_index, frame_index, inst);
+		page_table[page_index].Map(rw, page_index, frame_index, inst);
+	}
+}
+void printp(int p_flag){
+	if (p_flag==1){
+		for(int i=0; i<num_pages; i++){
+			// cout<<page_table[i].PRESENT<<" ";
+			if(page_table[i].PRESENT==-1){
+				cout<<"* ";
+			}
+			else if(page_table[i].PRESENT==1){
+				cout<<i<<":";
+				if(page_table[i].REFERENCED==1)cout<<"R"; else cout<<"-";
+				if(page_table[i].MODIFIED==1)cout<<"M"; else cout<<"-";
+				if(page_table[i].PAGEDOUT==1)cout<<"S"; else cout<<"- ";
+			}
+		}
+		cout<<"\n";
+	}	
+}
+void printf(int f_flag){
+	if (f_flag==1){
+		for(int i=0; i<num_frames; i++){
+			if(frametable[i]==-1){cout<<"* ";			}
+			else {
+				cout<<frametable[i]<<" ";
+			}				
+		}
+		cout<<"\n";
+	}	
 }
 
 
@@ -103,11 +179,33 @@ int main(int argc, char *argv[]){
 	cout<<"algo="<<algo<<" ovalue="<<ovalue<<" num_frames="<<num_frames<<endl;
 	
 	input_init(argv[argc-2]);	
-	rfile_init(argv[argc-1]);
-	// myrandom(10, ofs);	
-	
-	
-	
+	rfile_init(argv[argc-1]);		
+	frametable_init(num_frames);
+	fram2page_init(num_frames);
+	for (int task_idx=0; task_idx<tasks.size(); task_idx++){
+		cout<<"==> inst: "<<tasks[task_idx][0]<<" "<<tasks[task_idx][1]<<endl;
+		change(tasks[task_idx][0], tasks[task_idx][1], task_idx);
+		printp(p_flag);	
+		printf(f_flag);	
+	}
+
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
