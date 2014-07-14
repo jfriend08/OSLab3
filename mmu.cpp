9429a2fccedaeaf5985c9d1c19c946ba2a872652
jfriend08 <ys486@cornell.edu>
2014-07-14 13:20:41 -0400

    --	init input, init rfile --	myrandom function

--- mmu.cpp
+++ mmu.cpp
@@ -1,6 +1,8 @@
 /*
 Usage: ./mmu [-a<algo>] [-o<options>] [–f<num_frames>] inputfile randomfile
 --	detect output flag, algorithm methods, and number of frames
+--	init input, init rfile
+--	myrandom function
 
 */
 
@@ -23,10 +25,13 @@ Usage: ./mmu [-a<algo>] [-o<options>] [–f<num_frames>] inputfile randomfile
 
 using namespace std;
 vector<int> randvals;
+vector<int> tasks_tmp;
+vector<vector<int> > tasks;
 int num_frames=32, num_pages=64, O_flag=0, P_flag=0, F_flag=0, S_flag=0, p_flag=0, f_flag=0, a_flag=0; 
-int c, opterr=0;
+int c, opterr=0, n1, n2, ofs=0;
 // char *algo;
-string ovalue, frames, arg_tmp, algo="l";
+string ovalue, frames, arg_tmp, algo="l", fin_string;
+char fin_char;
 
 ////Function////
 int myrandom(int burst, int &index) { 	
@@ -35,7 +40,7 @@ int myrandom(int burst, int &index) {
 	// cout<<index<<":"<<randvals[index]<<endl;	
 	return 1 + (randvals[index] % burst);	
 }
-
+////Function////
 void flagAssign(string outputVal){
 	for (int i=0; i<outputVal.length(); i++){
 		if (outputVal[i]=='O'){O_flag=1;}
@@ -48,6 +53,35 @@ void flagAssign(string outputVal){
 	}
 	cout<<"O_flag="<<O_flag<<" P_flag="<<P_flag<<" F_flag="<<F_flag<<" S_flag="<<S_flag<<" p_flag="<<p_flag<<" f_flag="<<f_flag<<" a_flag="<<a_flag<<endl;
 }
+////Function////
+void input_init(string input1){
+	string line;
+	int found;
+	ifstream fin0 ( input1 );
+	if (!fin0.is_open()){cout<<"Cannot open the file0"<<endl;	}
+	else{
+		while(!fin0.eof()){
+			getline(fin0, line);
+			if (line[0]!='#' && line !=""){				
+				std::stringstream(line)>>n1>>n2;tasks_tmp.push_back(n1);tasks_tmp.push_back(n2);
+        		tasks.push_back(tasks_tmp);tasks_tmp.clear();
+			}					
+		}		
+	}
+
+	fin0.close();
+}
+////Function////
+void rfile_init(string input2){
+	ifstream fin ( input2 );
+	if (!fin.is_open()){cout<<"Cannot open the file1"<<endl;	}
+	else{
+		for (int i=0; !fin.eof();i++){
+			int temp;fin>>temp;randvals.push_back(temp);			
+		}
+	}
+}
+
 
 int main(int argc, char *argv[]){
 	while((c=getopt(argc,argv,"a:o:f:")) !=-1){
@@ -68,9 +102,12 @@ int main(int argc, char *argv[]){
 	}
 	cout<<"algo="<<algo<<" ovalue="<<ovalue<<" num_frames="<<num_frames<<endl;
 	
+	input_init(argv[argc-2]);	
+	rfile_init(argv[argc-1]);
+	// myrandom(10, ofs);	
 	
-
-
+	
+	
 
 	return 0;
 }
