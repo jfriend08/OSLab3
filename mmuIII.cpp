f430a001a7df790172b38b74bb4a9452b70c5e8d
jfriend08 <ys486@cornell.edu>
2014-07-16 02:51:51 -0400

    --	==> inst 27: 1 6 has issue

--- mmuIII.cpp
+++ mmuIII.cpp
@@ -7,6 +7,7 @@ Usage: ./mmu [-a<algo>] [-o<options>] [–f<num_frames>] inputfile randomfile
 --	use bitwise operator
 --	adding algo=r method, random generator is fine, need to add unmap method
 --	have better OO design
+--	==> inst 27: 1 6 has issue
 
 Todo:	replacement algorithms is needed
 
@@ -90,12 +91,14 @@ void printp(int p_flag, int* page_table, vector<int>* frame_table){
 	if (p_flag==1){
 		for(int i=0; i<num_pages; i++){
 			int control_bit=page_table[i];
-			control_bit>>=6;
-			if((control_bit & 8) !=8){				
+			control_bit>>=6;			
+			if((control_bit & 8) !=8 && (control_bit & 1) != 1){				
 				printf("* ");			
 			}
+			else if((control_bit & 1) == 1)cout<<"# ";
 			else if((control_bit & 8) == 8){
 				cout<<i<<":";
+				// cout<<i<<":"<<"control_bit:"<<control_bit;
 				if((control_bit & 4) == 4)cout<<"R"; else cout<<"-";
 				if((control_bit & 2) == 2)cout<<"M"; else cout<<"-";
 				if((control_bit & 1) == 1)cout<<"S "; else cout<<"- ";
@@ -150,37 +153,72 @@ public:
 		}
 	}
 	void Zero(int rw, int page_index, int frame_index, int inst){
+		workon_F->at(frame_index)=0;
 		string tmp="";
 		printf("%d: ZERO %4s %3d\n", inst ,tmp.c_str(), frame_index);	
 	} 
 	void Map(int rw, int page_index, int frame_index, int inst){
-		int value=workon_P[page_index];
+		int value=workon_P[page_index];		
 		printf("%d: MAP %5d %3d\n", inst ,page_index, frame_index);			
 		workon_F->at(frame_index)=page_index;
+		// cout<<"pre Map value/rw:"<<value<<"/"<<rw<<" page_index:"<<page_index<<" workon_P[page_index]"<<workon_P[page_index]<<endl;
 		if (rw==1){value|=896;}
-		else value|=768;
+		else if (rw==0) {value<<=13;value>>=13;value|=768;}
+		// cout<<"post Map value/rw:"<<value<<"/"<<rw<<endl;
 		value|=frame_index;
 		workon_P[page_index]=value;
 	}
-	void pageout(int rw, int page_index, int frame_index, int inst){}
-	void pagein(int rw, int page_index, int frame_index, int inst){}	
+	void Unmap(int page_index, int frame_index, int inst){
+		// cout<<"Unmap page_index:"<<page_index<<" frame_index:"<<frame_index<<endl;
+		int value=workon_P[workon_F->at(frame_index)];
+		// cout<<workon_F->at(frame_index)<<":"<<workon_P[workon_F->at(frame_index)]<<endl;
+		// cout<<"unmap value_pre:"<<value<<endl;
+		value>>=6;value<<=6;value^=512;workon_P[workon_F->at(frame_index)]=value;
+		// cout<<"unmap value_post:"<<value<<endl;
+		printf("%d: UNMAP %3d %3d\n", inst ,workon_F->at(frame_index), frame_index);			
+		// cout<<"workon_P[workon_F->at(frame_index)]"<<workon_P[workon_F->at(frame_index)]<<endl;
+	}
+	void Pageout(int rw, int page_index, int frame_index, int inst){
+		workon_P[workon_F->at(frame_index)]|=64;
+		printf("%d: OUT %5d %3d\n", inst ,workon_F->at(frame_index), frame_index);			
+	}
+	void Pagein(int rw, int page_index, int frame_index, int inst){}	
+	
 	Pager* pager;
 	int* workon_P;
 	vector<int>* workon_F;
 	void Process(){
-		// for (int i=0; i<64; i++){cout<<workon_P[i]<<endl;}
+		
 		// for (int i=0; i<num_frames; i++){cout<<"FrameTable:"<<workon_F->at(i)<<endl;}
-		for (int task_idx=0; task_idx<24; task_idx++){
+		for (int task_idx=0; task_idx<40; task_idx++){
 			cout<<"==> inst: "<<tasks[task_idx][0]<<" "<<tasks[task_idx][1]<<endl;
 			int rw=tasks[task_idx][0];
 			int page_index=tasks[task_idx][1];
-			if (workon_P[tasks[task_idx][1]]==0){				
+			////pager->frameFull() != -1 --> no page fault////
+			if (workon_P[tasks[task_idx][1]]==0 && pager->frameFull()!=-1){				
 				int frame_index=pager->Change(workon_P, workon_F);	
 				Zero(rw, page_index, frame_index, task_idx);
-				Map(rw, page_index, frame_index, task_idx);			
-				
+				Map(rw, page_index, frame_index, task_idx);							
 			}
-			
+			else if (pager->frameFull()==-1 && workon_P[page_index] ==0){
+				int frame_index = pager->Change(workon_P, workon_F);	
+				Unmap(page_index,frame_index, task_idx);							
+				if ((workon_P[workon_F->at(frame_index)] & 128) == 128){ Pageout (rw, page_index, frame_index, task_idx);}
+				Zero(rw, page_index, frame_index, task_idx);
+				Map(rw, page_index, frame_index, task_idx);							
+
+			}
+			else if (pager->frameFull()==-1 && workon_P[page_index] !=0 && rw == 0){}
+			////pager->frameFull() == -1 --> page fault////			
+			else if (pager->frameFull()==-1){				
+				int frame_index = pager->Change(workon_P, workon_F);	
+				Unmap(page_index,frame_index, task_idx);							
+				if ((workon_P[workon_F->at(frame_index)] & 128) == 128){ Pageout (rw, page_index, frame_index, task_idx);}
+				Zero(rw, page_index, frame_index, task_idx);
+				Map(rw, page_index, frame_index, task_idx);							
+			}
+			// for (int i=0; i<64; i++){cout<<i<<":"<<workon_P[i]<<" ";}			
+			// cout<<endl;
 			printp(p_flag, workon_P, workon_F);	
 			printf(f_flag, workon_P, workon_F);	
 		}
